ebc367ca0b39242fb7d25bb1a7e9b83355da2c76
Aleksei Danilov <danilovdev@gmail.com>
2018-06-27 00:25:34 +0800

    shuffle algorithms

--- Playgrounds/ShuffleArray.playground/Contents.swift
+++ Playgrounds/ShuffleArray.playground/Contents.swift
@@ -1,72 +1,130 @@
-//: Playground - noun: a place where people can play
 
 import Foundation
 
-//var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-//var strings = ["one", "two", "three", "four", "five"]
-//
-//func shuffle<T>(array: inout [T]) -> [T] {
-//    var result = [T]()
-//    for _ in array {
-//        let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
-//        result.append(array.remove(at: randomIndex))
-//    }
-//    return result
-//}
-//
-//print(strings) // ["one", "two", "three", "four", "five"]
-//print(shuffle(array: &strings)) // ["five", "three", "two", "one", "four"]
-//
-//let randomArray = shuffle(array: &array)
-//print(array) // []
-//print(randomArray) // [3, 4, 6, 8, 1, 2, 10, 7, 9, 5]
-
-import Foundation
-
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-func shuffle<T>(array: inout [T]) -> [T] {
-    var length = array.count
+func shuffle(array: [Int]) -> [Int] {
+    var arrayCopy = array
+    var randomArray = [Int]()
     for _ in array {
-        let randomIndex = Int(arc4random_uniform(UInt32(length)))
-        if (randomIndex != (length - 1)) {
-            array.swapAt(length - 1, randomIndex)
-        }
-        length -= 1
+        let randomIndex = Int(arc4random_uniform(UInt32(arrayCopy.count)))
+        let removedValue = arrayCopy.remove(at: randomIndex)
+        randomArray.append(removedValue)
     }
-    return array
+    return randomArray
 }
+
+//let shuffledArray = shuffle(array: array)
+//print(array) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(shuffledArray) //[7, 6, 9, 2, 8, 4, 3, 1, 5, 10]
 
 extension Array {
     
-    mutating func shuffle() -> [Element] {
-        var length = self.count
+    func shuffle() -> [Element] {
+        var arrayCopy = self
+        var result = [Element]()
         for _ in self {
-            let randomIndex = Int(arc4random_uniform(UInt32(length)))
-            if () {
-                
-            }
+            let randomIndex = Int(arc4random_uniform(UInt32(arrayCopy.count)))
+            let removedItem = arrayCopy.remove(at: randomIndex)
+            result.append(removedItem)
         }
-        return self
+        return result
     }
 }
 
-print(array) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(shuffle(array: &array)) // [3, 4, 6, 8, 1, 2, 10, 7, 9, 5]
+//let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(array2) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(array2.shuffle()) //[5, 9, 6, 10, 2, 7, 1, 3, 8, 4]
 
-//extension Array {
-//
-//    mutating func shuffle() -> [Element] {
-//        var result = [Element]()
-//        for _ in self {
-//            let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
-//            result.append(self.remove(at: randomIndex))
-//        }
-//        return result
-//    }
-//}
-//
-//print(array) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//print(array.shuffle()) // [2, 5, 4, 1, 7, 9, 3, 10, 8, 6]
+//let strings = ["one", "two", "three", "four", "five"]
+//print(strings) //["one", "two", "three", "four", "five"]
+//print(strings.shuffle()) //["five", "four", "one", "three", "two"]
+
+extension Array {
+    
+    mutating func shuffleInline() {
+        var result = [Element]()
+        for _ in self {
+            let randomIndex = Int(arc4random_uniform(UInt32(self.count)))
+            let removedItem = self.remove(at: randomIndex)
+            result.append(removedItem)
+        }
+        self = result
+    }
+}
+
+//var array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(array3) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//array3.shuffleInline()
+//print(array3) //[3, 1, 4, 6, 2, 5, 9, 10, 7, 8]
+
+
+func shuffleModern(array: [Int]) -> [Int] {
+    var length = array.count
+    var arrayCopy = array
+    for _ in array {
+        let randomIndex = Int(arc4random_uniform(UInt32(length)))
+        if length - 1 != randomIndex {
+            arrayCopy.swapAt(length - 1, randomIndex)
+        }
+        length -= 1
+    }
+    return arrayCopy
+}
+
+//let array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(array4) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//print(shuffleModern(array: array4)) //[9, 6, 7, 10, 2, 8, 1, 4, 3, 5]
+
+
+func shuffleModern<T>(array: [T]) -> [T] {
+    var length = array.count
+    var arrayCopy = array
+    for _ in array {
+        let randomIndex = Int(arc4random_uniform(UInt32(length)))
+        if length - 1 != randomIndex {
+            arrayCopy.swapAt(length - 1, randomIndex)
+        }
+        length -= 1
+    }
+    return arrayCopy
+}
+
+//let strings2 = ["one", "two", "three", "four", "five"]
+//print(strings2) //["one", "two", "three", "four", "five"]
+//print(shuffleModern(array: strings2)) //["five", "four", "one", "three", "two"]
+
+extension Array {
+    
+    func shuffleModern() -> [Element] {
+        var arrayCopy = self
+        var length = arrayCopy.count
+        for _ in arrayCopy {
+            let randomIndex = Int(arc4random_uniform(UInt32(length)))
+            if length - 1 != randomIndex {
+                arrayCopy.swapAt(length - 1, randomIndex)
+            }
+            length -= 1
+        }
+        return arrayCopy
+    }
+    
+    mutating func shuffleModernInline() {
+        var length = self.count
+        for _ in self {
+            let randomIndex = Int(arc4random_uniform(UInt32(length)))
+            if length - 1 != randomIndex {
+                swapAt(length - 1, randomIndex)
+            }
+            length -= 1
+        }
+    }
+}
+
+var array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(array5) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(array5.shuffleModern()) // [2, 6, 10, 5, 4, 7, 3, 8, 9, 1]
+array5.shuffleModernInline()
+print(array5) // [10, 6, 2, 3, 1, 8, 5, 4, 7, 9]
+
 
