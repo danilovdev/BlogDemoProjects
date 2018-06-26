831210394945aedae85581c80bc8d87831262f82
Aleksei Danilov <danilovdev@gmail.com>
2018-06-26 12:53:59 +0800

    defer demo

--- Playgrounds/DeferStatement.playground/Contents.swift
+++ Playgrounds/DeferStatement.playground/Contents.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 
 func testDefer1() {
     defer {
@@ -8,7 +8,11 @@ func testDefer1() {
     print("Normal step 2")
 }
 
-//testDefer1()
+testDefer1()
+
+//Normal step 1
+//Normal step 2
+//Defer step 3
 
 func testDefer2() {
     print("Normal step 1")
@@ -24,7 +28,13 @@ func customFunc() {
     }
 }
 
-//testDefer2()
+testDefer2()
+
+//Normal step 1
+//Custom step 1
+//Custom step 2
+//Custom defer step 3
+//Normal step 2
 
 func testMultipleDefer() {
     print("Normal step 1")
@@ -40,7 +50,13 @@ func testMultipleDefer() {
     print("Normal step 2")
 }
 
-//testMultipleDefer()
+testMultipleDefer()
+
+//Normal step 1
+//Normal step 2
+//Defer step 3
+//Defer step 2
+//Defer step 1
 
 func testDefer3() {
     for i in 1...5 {
@@ -52,5 +68,59 @@ func testDefer3() {
     }
 }
 
-//testDefer3()
+testDefer3()
+
+//Normal step iteration begins 1
+//Normal step iteration ends 1
+//Defer step 1
+//Normal step iteration begins 2
+//Normal step iteration ends 2
+//Defer step 2
+//Normal step iteration begins 3
+//Normal step iteration ends 3
+//Defer step 3
+//Normal step iteration begins 4
+//Normal step iteration ends 4
+//Defer step 4
+//Normal step iteration begins 5
+//Normal step iteration ends 5
+//Defer step 5
+
+enum FileError: Error {
+    case fileNotFound(reason: String)
+    case cannotWriteToFile(reason: String)
+}
+
+func writeToFile(filename: String, text: String) throws {
+    if let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        
+        
+        
+        let path = directory.appendingPathComponent(filename).absoluteString
+        
+        guard let file = FileHandle(forUpdatingAtPath: path) else {
+            print("File open failed")
+            throw FileError.fileNotFound(reason: "File not found")
+        }
+        
+        defer {
+            print("Close file...")
+            file.closeFile()
+        }
+        
+        let fileURL = directory.appendingPathComponent(filename)
+        
+        do {
+            try text.write(to: fileURL, atomically: false, encoding: .utf8)
+        }
+        catch {
+            throw FileError.cannotWriteToFile(reason: "Cannot write to file")
+        }
+    }
+}
+
+
+
+
+
 
